e9ee38674d19d893db7f0d23f3ddea30f2c0a19e
innomatrics <innomatricstech@gmail.com>
2026-01-08 13:05:17 +0530

    first commit

--- src/Pages/Invoice.jsx
+++ src/Pages/Invoice.jsx
@@ -137,30 +137,69 @@ const Invoice = () => {
   };
 
   // Number to words conversion
-  const numberToWords = (num) => {
-    const a = [
-      '', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine',
-      'Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen',
-      'Seventeen', 'Eighteen', 'Nineteen'
-    ];
-    
-    const b = [
-      '', '', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety'
-    ];
-    
-    if (num === 0) return 'Zero Rupees Only';
-    
-    const convert = (n) => {
-      if (n < 20) return a[n];
-      const digit = n % 10;
-      if (n < 100) return b[Math.floor(n / 10)] + (digit ? ' ' + a[digit] : '');
-      if (n < 1000) return a[Math.floor(n / 100)] + ' Hundred' + (n % 100 ? ' ' + convert(n % 100) : '');
-      return '';
-    };
-    
-    const words = convert(Math.floor(num));
-    return words + ' Rupees Only';
+ const numberToWords = (amount) => {
+  if (amount === null || amount === undefined || isNaN(amount)) {
+    return "Zero Rupees Only";
+  }
+
+  const ones = [
+    "", "One", "Two", "Three", "Four", "Five",
+    "Six", "Seven", "Eight", "Nine", "Ten",
+    "Eleven", "Twelve", "Thirteen", "Fourteen",
+    "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+  ];
+
+  const tens = [
+    "", "", "Twenty", "Thirty", "Forty",
+    "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
+  ];
+
+  const convertTwoDigits = (num) => {
+    if (num < 20) return ones[num];
+    return tens[Math.floor(num / 10)] + (num % 10 ? " " + ones[num % 10] : "");
   };
+
+  const convertNumber = (num) => {
+    let result = "";
+
+    if (num >= 10000000) {
+      result += convertNumber(Math.floor(num / 10000000)) + " Crore ";
+      num %= 10000000;
+    }
+
+    if (num >= 100000) {
+      result += convertNumber(Math.floor(num / 100000)) + " Lakh ";
+      num %= 100000;
+    }
+
+    if (num >= 1000) {
+      result += convertNumber(Math.floor(num / 1000)) + " Thousand ";
+      num %= 1000;
+    }
+
+    if (num >= 100) {
+      result += ones[Math.floor(num / 100)] + " Hundred ";
+      num %= 100;
+    }
+
+    if (num > 0) {
+      result += convertTwoDigits(num);
+    }
+
+    return result.trim();
+  };
+
+  const rupees = Math.floor(amount);
+  const paise = Math.round((amount - rupees) * 100);
+
+  let words = convertNumber(rupees) + " Rupees";
+
+  if (paise > 0) {
+    words += " and " + convertTwoDigits(paise) + " Paise";
+  }
+
+  return words + " Only";
+};
 
   // Return order handler
   const handleReturnOrder = () => {
